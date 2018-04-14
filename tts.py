693da979e37a2867762799f52098affb76ae5c07
Kasper Steensig <kasper.steensig@gmail.com>
2018-04-14 19:58:36 +0200

    added tts

---
+++ tts.py
@@ -0,0 +1,16 @@
+from gtts import gTTS
+
+class TextToSpeech:
+    def __init__(dest, filename, title, top_comment):
+        self.dest = dest
+        self.filename = filename
+        self.title = title
+        self.top_comment = top_comment
+
+def tts(dest, filename, title, top_comment):
+    title_tts = gTTS(text=title, lang='en')
+    top_comment_tts = gTTS(text=top_comment, lang='en')
+    title_tts.save(dest + "/" + filename + ".mp3")
+    top_comment_tts.save(dest + "/" + filename + ".mp3")
+
+    return TextToSpeech(dest, filename, title, top_comment)    
