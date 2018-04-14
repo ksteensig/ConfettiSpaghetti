9873e9842defe77fdb5df290ba88d5b59764899c
luraman <ponifi3d@gmail.com>
2018-04-14 12:57:37 +0200

    scrape transform done

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 import praw
 import itertools as it
 import keys
+from imgur import imgur_parse_link
 
 _SORTINGS = ["controversial", "gilded", "hot", "new", "rising", "top"]
 _TIMEFILTERS = ["day", "hour", "month", "week", "year", "all"]
@@ -23,7 +24,7 @@ def extract_submission(sm):
         return None
 
 def extract_imgur(sm):
-    return sm.url
+    return imgur_parse_link(sm.url)
 
 def extract_redditv(sm):
     return sm.media["reddit_video"]["fallback_url"]
@@ -32,9 +33,11 @@ def submission_transform(sm):
     return (sm.title, extract_submission(sm))
 
 def extract_links(subreddit, resultsize = 100, **generator_kwargs):
+    #Force None as default limit
+    generator_kwargs["limit"] = generator_kwargs.get("limit")
     return it.islice(filter(lambda sm: sm[1] is not None, map(submission_transform, subreddit_iterator(subreddit, **generator_kwargs))), resultsize)
 
-results = extract_links("funny", limit = None)
+results = extract_links("wholesomememes", resultsize = 10)
 
 for sm in results:
     print(sm)
