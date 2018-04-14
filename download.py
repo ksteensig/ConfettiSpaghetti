28252c32f13e7d450b138779edc5247b7ed38bf5
Kasper Steensig <kasper.steensig@gmail.com>
2018-04-14 13:14:49 +0200

    better decoupling

--- download.py
+++ download.py
@@ -1,19 +1,21 @@
 import requests
 
 class Video:
-    def __init__(self, dest, title, src):
+    def __init__(self, dest, title, src, filename):
         self.dest = dest
         self.title = title
         self.link = src
-        self.filename = title.replace(' ', '')
+        self.filename = filename
 
-def download_file(dest, title, src):
+def download_file(src):
     r = requests.get(src)
     if r.status_code == 200:
-        vid = Video(dest, title, src)
-        with open(dest + title.replace(' ', '') + '.mp4', 'wb') as f:
-            f.write(r.content)
-        return vid
+        return r.content
     return None
+
+def save_file(dest, title, src, filename, content):
+    with open(dest + filename + '.mp4'):
+        f.write(content)
+    return Video(dest, title, src, filename)
 
 #vid = download_file('./', '(O) ^ (O)', 'https://i.imgur.com/KhOSXiT.mp4')
