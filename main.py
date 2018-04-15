d37eb0aa10268f082d7767858261812998193fa2
luraman <ponifi3d@gmail.com>
2018-04-15 06:27:41 +0200

    Add user_interface

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from download import Video, download_file, save_file
+from prawcore import exceptions as prawex
 from tts import tts
 from scrape import extract_links, sub2save, sub2sound
 from convert import convert
+from keys import reddit
+import re
 import itertools as it
 from operator import itemgetter
-import time
+from datetime import datetime
 
 def scrape(dest, subreddit, **generator_kwargs):
     videos = []
@@ -23,9 +26,31 @@ def scrape(dest, subreddit, **generator_kwargs):
 
     sounds = it.starmap(tts, texts)
 
-    return videos, sounds, dest
+    convert(videos, sounds, dest)
 
-ts = str(time.time())
+def basic_scrape(subreddit):
+    ts = str(datetime.now())
+    scrape("./" + subreddit + "/" + ts, subreddit)
 
-vids, snds, dest = scrape("./totallynotrobots/" + ts, "totallynotrobots")
-convert(vids, snds, dest)
+def user_interface():
+    subreddit = None
+    print("Welcome to Viddit!")
+    while True:
+        print("Please enter the name of the subreddit you wish to compile or type 'exit':")
+        subreddit = input("> ")
+        if subreddit == "exit":
+            return
+        subreddit = re.sub(r'^r\/', '', subreddit)
+        try:
+            results = reddit.subreddits.search_by_name(subreddit, include_nsfw=False, exact=True)
+        except prawex.NotFound:
+            print("Sorry, that subreddit doesn't exist")
+            continue
+        if not results:
+            print("Sorry, that subreddit isn't safe for children ages 6 or under.")
+        break
+
+    print("Great! Now let Python do its magic. This might take a while...")
+    basic_scrape(subreddit)
+
+user_interface()
